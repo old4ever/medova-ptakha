2fe465310a0a0b9d506ee648b51babcc11b7f7d9
old4ever <git@old4ever.simplelogin.com>
2023-12-24 16:40:30 -0500

    Fixed index mismatch in authors and replaced arrow glyphs with SVGs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,32 @@ const Volume: React.FC<VolumeProps> = ({
   );
 };
 
+const LeftArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="32"
+      width="16"
+      viewBox="0 0 256 512"
+    >
+      <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z" />
+    </svg>
+  );
+};
+
+const RightArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="32"
+      width="16"
+      viewBox="0 0 256 512"
+    >
+      <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
+    </svg>
+  );
+};
+
 export default function Home() {
   // const volumesCount = 5;
 
@@ -163,7 +189,7 @@ export default function Home() {
                     }
                   }}
                 >
-                  <span className="text-4xl"></span>
+                  <LeftArrow />
                 </button>
                 <div className="wrapper grid auto-cols-[30%] grid-flow-col gap-4 overflow-x-scroll">
                   <Volume isLead={false} img={Volume2} volumeNumber={2} />
@@ -187,7 +213,7 @@ export default function Home() {
                     }
                   }}
                 >
-                  <span className="text-4xl"></span>
+                  <RightArrow />
                 </button>
               </div>
             </div>
@@ -227,14 +253,14 @@ export default function Home() {
                 }
               }}
             >
-              <span className="text-4xl"></span>
+              <LeftArrow />
             </button>
             <div className="wrapper grid auto-cols-[15%] grid-flow-col gap-4 overflow-x-scroll">
-              {Array.from(Array(15)).map(() => {
+              {Array.from(Array(15)).map((_el, index) => {
                 return (
                   <div
                     className="flex flex-col items-center justify-center"
-                    key={crypto.randomUUID()}
+                    key={index}
                   >
                     <Image
                       className="aspect-square h-full w-full rounded-full border border-black object-cover object-top"
@@ -243,7 +269,7 @@ export default function Home() {
                       alt="Profile Picture"
                       src={PlaceholderAvatar}
                     />
-                    <div>{`Автор ${Math.floor(Math.random() * 10)}`}</div>
+                    <div>{`Автор ${index}`}</div>
                   </div>
                 );
               })}
@@ -264,7 +290,7 @@ export default function Home() {
                 }
               }}
             >
-              <span className="text-4xl"></span>
+              <RightArrow />
             </button>
           </div>
           <div className=""></div>
