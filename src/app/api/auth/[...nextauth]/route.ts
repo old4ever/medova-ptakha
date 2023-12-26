5ac1f5ed1e49ad30d39bb2c35fff41a960367bec
old4ever <git@old4ever.simplelogin.com>
2023-12-26 17:31:25 -0500

    trying to fix next-js api requirements

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,6 @@
+import { options } from "~/app/api/auth/[...nextauth]/options";
 import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { db } from "~/server/db";
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-
-const handler: NextAuthOptions = NextAuth({
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ account, profile }) {
-      if (!profile?.email) {
-        throw new Error("No profile");
-      }
-
-      await db.user.upsert({
-        where: { email: profile.email },
-        create: {
-          email: profile.email,
-          name: profile.name,
-          avatar: profile.image,
-        },
-        update: { avatar: profile.image },
-      });
-
-      return true;
-    },
-  },
-}) as NextAuthOptions;
+const handler = NextAuth(options) as object;
 
 export { handler as GET, handler as POST };
