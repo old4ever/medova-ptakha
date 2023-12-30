0e9396e34a4e9bccfafcbb1ed782d0472210c7c8
old4ever <git@old4ever.simplelogin.com>
2023-12-30 14:13:14 -0500

    fix: Title view on mobile

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -21,20 +21,20 @@ const Title = ({ subheading }: { subheading?: string }) => {
   return (
     <>
       <div className="flex items-center justify-center bg-primary pb-9 pt-9 font-fancy">
-        <span className="pr-5 pt-4 text-6xl sm:pr-6 sm:text-7xl md:pr-7 md:text-8xl lg:pr-8 lg:text-9xl">
+        <span className="xsm:text-6xl pr-5 pt-4 text-4xl sm:pr-6 sm:text-7xl md:pr-7  lg:pr-8">
           Медова
         </span>
-        <div className="min-w-[6rem] sm:min-w-[7rem] md:min-w-[8rem] lg:min-w-[9rem]">
+        <div className="xsm:min-w-[8rem] min-w-[6rem] sm:min-w-[9rem] md:min-w-[10rem] lg:min-w-[11rem]">
           <img className="w-full" alt="Logo" src="/logo-m.svg"></img>
         </div>
-        <span className="pl-1 pt-4 text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
+        <span className="xsm:text-6xl pl-2 pt-4 text-4xl sm:text-7xl ">
           Птаха
         </span>
       </div>
       {subheading && (
         <div className="flex items-center justify-center bg-primary font-fancy">
-          {/* <span className="mt-[-3rem] pl-16 text-4xl sm:ml-6 sm:mt-[-2rem] sm:pb-4 sm:pl-0 sm:text-5xl md:ml-7 md:text-6xl lg:ml-8 lg:text-7xl"> */}
-          <span className="mt-[-1rem] pb-4 pl-4 text-4xl sm:text-5xl md:ml-7 md:text-6xl lg:ml-8 lg:text-7xl">
+          {/* <span className="mt-[-3rem] pl-16 text-4xl sm:ml-6 sm:mt-[-2rem] sm:pb-4 sm:pl-0 sm:text-7xl md:ml-7  lg:ml-8"> */}
+          <span className="mt-[-1rem] pb-4 pl-4 text-4xl sm:text-5xl md:ml-7 lg:ml-8">
             {subheading}
           </span>
         </div>
