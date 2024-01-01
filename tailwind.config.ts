22a79b7939116ee7d59fa39d94647f2bc1861c46
old4ever <git@old4ever.simplelogin.com>
2024-01-01 15:22:16 -0500

    refactor: shadcn-ui reinstall and tweaks to tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,45 +1,10 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-// export default {
-//   content: ["./src/**/*.tsx"],
-//   theme: {
-//     extend: {
-//       // fontFamily: {
-//       //   sans: ["var(--font-sans)", ...fontFamily.sans],
-//       // },
-//       fontSize: {
-//         sm: "0.750rem",
-//         base: "1rem",
-//         xl: "1.333rem",
-//         "2xl": "1.777rem",
-//         "3xl": "2.369rem",
-//         "4xl": "3.158rem",
-//         "5xl": "4.210rem",
-//       },
-//       fontFamily: {
-//         heading: "JetBrainsMono",
-//         body: "Rubik",
-//       },
-//       fontWeight: {
-//         normal: "400",
-//         bold: "700",
-//       },
-//       colors: {
-//         text: "#05050a",
-//         background: "#f5f5fa",
-//         primary: "#FFD700",
-//         secondary: "#f5be75",
-//         accent: "#a7f356",
-//       },
-//     },
-//   },
-//   plugins: [],
-// } satisfies Config;
-
 export default {
-  darkMode: "class",
+  darkMode: ["class"],
   content: ["./src/**/*.tsx"],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -52,12 +17,10 @@ export default {
       screens: { xsm: "380px" },
       fontFamily: {
         // heading: "JetBrainsMono",
-        heading: "Alegreya Sans",
-        body: "Rubik",
-        fancy: "KrasnopysChebanyk",
-      },
-      maxWidth: {
-        "8xl": "1408px",
+        // heading: "Alegreya Sans",
+        // body: "Rubik",
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+        fancy: "var(--font-fancy)",
       },
       colors: {
         border: "hsl(var(--border))",
@@ -102,18 +65,18 @@ export default {
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-hight)" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
-          from: { height: "var(--radix-accordion-content-hight)" },
+          from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
       },
       animation: {
-        "accordion-up": "accordion-up 0.2 ease-out",
-        "accordion-down": "accordion-down 0.2 ease-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
